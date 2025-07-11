6629daba8905f6090a8d0d7df1dd94bd545260b6
jyung <huntthetux@gmail.com>
2025-07-11 13:20:15 +0800

    checkpoint at 2025-07-11T13:20:15+0800

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,19 +1,61 @@
 fn main() {
+    let mut inputs: Vec<String> = Vec::new();
     let args: Vec<String> = std::env::args().collect();
-    let lens = args.len();
-    let title = args[1].clone();
-    // let mut content = String::from("default content");
-    //
-    // if lens > 2 {
-    //     content = args[2].clone();
-    // }
+    let mut ok = args[1].clone() == "create";
 
-    let content = if lens > 2 {
-        println!("has more than 2");
-        args[2].clone()
-    } else {
-        String::from("default content")
-    };
+    while ok {
+        let len = inputs.len();
 
-    println!("todo title: {}, content: {}", title, content);
+        if len == 0 {
+            println!("Please input todo title");
+
+            let mut title = String::new();
+
+            std::io::stdin()
+                .read_line(&mut title)
+                .expect("read line failed");
+
+            if title.is_empty() {
+                println!("emptyyyy");
+                continue;
+            }
+
+            inputs.push(title.trim().to_string());
+        } else if len == 1 {
+            println!("Please input todo content");
+
+            let mut content = String::new();
+
+            std::io::stdin()
+                .read_line(&mut content)
+                .expect("read line failed");
+
+            if content.is_empty() {
+                continue;
+            }
+
+            inputs.push(content.trim().to_string());
+        } else {
+            println!("title: [{}]", inputs[0].clone());
+            println!("content: [{}]", inputs[1].clone());
+            println!("Are you sure to crate this todo? (y/n)");
+
+            let mut sure = String::new();
+
+            std::io::stdin()
+                .read_line(&mut sure)
+                .expect("read line failed");
+
+            if sure.trim().to_lowercase() != "n" {
+                ok = false;
+            } else {
+                inputs.clear();
+            }
+        }
+    }
+
+    let title = inputs[0].clone();
+    let content = inputs[1].clone();
+
+    println!("create todo title: {}, content: {}", title, content);
 }
