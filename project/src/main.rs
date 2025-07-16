c6572085accd9fa57524edc3792d93daf9866caa
jyung <huntthetux@gmail.com>
2025-07-16 22:34:35 +0800

    checkpoint at 2025-07-16T22:34:35+0800

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,61 +1,90 @@
+// fn main() {
+//     let s = "hello world";
+//
+//     println!("{}", &s[..4]);
+// }
+
 fn main() {
+    let mut todos: Vec<String> = Vec::new();
+    todos.push(String::from("learn rust"));
+    todos.push(String::from("work"));
+    todos.push(String::from("play"));
+
     let mut inputs: Vec<String> = Vec::new();
     let args: Vec<String> = std::env::args().collect();
-    let mut ok = args[1].clone() == "create";
 
-    while ok {
-        let len = inputs.len();
+    match args[1].clone().as_str() {
+        "create" => {
+            loop {
+                let len = inputs.len();
+                let mut ok = true;
 
-        if len == 0 {
-            println!("Please input todo title");
+                if len == 0 {
+                    println!("Please input todo title");
 
-            let mut title = String::new();
+                    let mut title = String::new();
 
-            std::io::stdin()
-                .read_line(&mut title)
-                .expect("read line failed");
+                    std::io::stdin()
+                        .read_line(&mut title)
+                        .expect("read line failed");
 
-            if title.is_empty() {
-                println!("emptyyyy");
-                continue;
+                    if title.is_empty() {
+                        println!("emptyyyy");
+                        continue;
+                    }
+
+                    inputs.push(title.trim().to_string());
+                } else if len == 1 {
+                    println!("Please input todo content");
+
+                    let mut content = String::new();
+
+                    std::io::stdin()
+                        .read_line(&mut content)
+                        .expect("read line failed");
+
+                    if content.is_empty() {
+                        continue;
+                    }
+
+                    inputs.push(content.trim().to_string());
+                } else {
+                    println!("title: [{}]", inputs[0].clone());
+                    println!("content: [{}]", inputs[1].clone());
+                    println!("Are you sure to crate this todo? (y/n)");
+
+                    let mut sure = String::new();
+
+                    std::io::stdin()
+                        .read_line(&mut sure)
+                        .expect("read line failed");
+
+                    if sure.trim().to_lowercase() != "n" {
+                        ok = false;
+                    } else {
+                        inputs.clear();
+                    }
+
+                    if !ok {
+                        break;
+                    }
+                }
             }
 
-            inputs.push(title.trim().to_string());
-        } else if len == 1 {
-            println!("Please input todo content");
+            let title = inputs[0].clone();
+            let content = inputs[1].clone();
 
-            let mut content = String::new();
+            println!("create todo title: {}, content: {}", title, content);
+        }
 
-            std::io::stdin()
-                .read_line(&mut content)
-                .expect("read line failed");
-
-            if content.is_empty() {
-                continue;
-            }
-
-            inputs.push(content.trim().to_string());
-        } else {
-            println!("title: [{}]", inputs[0].clone());
-            println!("content: [{}]", inputs[1].clone());
-            println!("Are you sure to crate this todo? (y/n)");
-
-            let mut sure = String::new();
-
-            std::io::stdin()
-                .read_line(&mut sure)
-                .expect("read line failed");
-
-            if sure.trim().to_lowercase() != "n" {
-                ok = false;
-            } else {
-                inputs.clear();
+        "l" | "list" => {
+            for todo in todos {
+                println!("todo title: {}", todo);
             }
         }
+
+        _ => {
+            println!("unknown command")
+        }
     }
-
-    let title = inputs[0].clone();
-    let content = inputs[1].clone();
-
-    println!("create todo title: {}, content: {}", title, content);
 }
